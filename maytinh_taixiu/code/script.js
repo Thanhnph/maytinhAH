e5334d67e23cac08839de9dd938f251e648ec940
Thanhnph <83392023+Thanhnph@users.noreply.github.com>
2021-09-18 01:00:38 +0700

    Add files via upload

---
+++ maytinh_taixiu/code/script.js
@@ -0,0 +1,45 @@
+const numbers = document.getElementsByClassName('btn');
+const result = document.getElementById("result");
+const logHistory = document.getElementById("history");
+let history = [];
+let expressionData = "";
+let resultData = "";
+
+for (let number of numbers) {
+    number.addEventListener('click', function () {
+        result.innerHTML += this.value
+    });
+}
+
+function equal() {
+    let res = result.innerHTML
+    let output = eval(res)
+    expressionData = res
+    resultData = output
+    history.push({ expression: expressionData, result: resultData });
+    showHistory()
+    result.innerHTML = output
+}
+
+function clean() {
+    result.innerHTML = " "
+}
+
+function undo() {
+    let res = result.innerHTML
+    result.innerHTML = res.substring(0, res.length - 1);
+}
+
+function showHistory() {
+    let log = ""
+    for (let key in history) {
+        log += history[key].expression + " = " + history[key].result + "<br>" + "<br>"
+    }
+    logHistory.innerHTML = log
+}
+
+// Index History
+let indexHistory = document.getElementsByClassName("fa-history")
+indexHistory[0].addEventListener("click", function () {
+    logHistory.classList.toggle("indexHistory")
+})
